fcaf1d636fda5c9732f6bd735a550ea4eb2bba85
Gabr12el <98735582+Gabr12el@users.noreply.github.com>
2022-04-09 13:32:37 +0400

    add test

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -8,11 +8,11 @@ public class StatisticsService {
      * @return - index of first max value
      */
     public long findMax(long[] incomes) {
-        long current_max_index = 0;
+        long current_max_index = 0;//вот не используется он(
         long current_max = incomes[0];
         for (long income : incomes)
             if (current_max < income)
-                current_max = income;
+                current_max = income;//некачественный код, но менять нельзя
         return current_max;
     }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,12 +9,17 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxMiddle() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 35, 8, 6, 11, 11, 12};
+        long expected = 35;
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
